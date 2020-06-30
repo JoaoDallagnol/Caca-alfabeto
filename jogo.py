e2e4572b4e6d15ebd0878841147e2f3253675054
Joao V <jvdallagnol2001@gmail.com>
2020-06-30 20:52:16 -0300

    Aplicando os random nas imagens

--- jogo.py
+++ jogo.py
@@ -21,9 +21,11 @@ a = pygame.image.load("assets/a_icon.png")
 a_largura = 86
 a_altura = 86
 
+
 b = pygame.image.load("assets/b_icon.png")
 b_largura = 86
 b_altura = 86
+
 
 c = pygame.image.load("assets/c_icon.png")
 c_largura = 79
@@ -37,101 +39,119 @@ dois = pygame.image.load("assets/dois_icon.png")
 dois_largura = 68 
 dois_altura = 68
 
+
 tres = pygame.image.load("assets/tres_icon.png")
 tres_largura = 76
 tres_altura = 78
 
+
 fundo = pygame.image.load("assets/fundo.jpg")
 
 letras_numero = [a,b,c,um,dois,tres]
-for item in letras_numero:
-    item = item
-
-    def mostrarCesta(x, y):
-        gameDisplay.blit(cesta, (x, y))
-
-    def mostrarAlfabeto(x, y):
-        gameDisplay.blit(item (x, y))
-
-    def text_objects(text, font):
-        textSurface = font.render(text, True, white)
-        return textSurface, textSurface.get_rect()
 
 
-    def message_display(text):
-        largeText = pygame.font.Font("freesansbold.ttf", 115)
-        TextSurf, TextRect = text_objects(text, largeText)
-        TextRect.center = (tela_largura/2, tela_altura/2)
-        gameDisplay.blit(TextSurf, TextRect)
-        pygame.display.update()
-        time.sleep(3)
-        game_loop()
+def mostrarCesta(x, y):
+    gameDisplay.blit(cesta, (x, y))
+
+def mostrarAlfabeto(objeto,x, y):
+    gameDisplay.blit(objeto, (x, y))
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, branco)
+    return textSurface, textSurface.get_rect()
 
 
-    def dead():
-        message_display("Você Morreu")
-
-
-    def escrePlacar(contador):
-        font = pygame.font.SysFont(None, 45)
-        text = font.render("Desvios: "+str(contador), True, white)
-        gameDisplay.blit(text, (10, 30))
-
-
-    def game_loop():
-        cesta_posicaoX = 350
-        cesta_posicaoY = 450
-        movimentoX = 0
-        item_speed = 3
-        item_posicaoX = random.randrange(0, tela_largura)
-        item_posicaoY = -250
-        desvios = 0
-
-        while True:
-            # inicio - Interação do usuário
-            # event.get do pygame, devolve uma lista de eventos da janela
-            for evento in pygame.event.get():
-                if evento.type == pygame.QUIT:
-                    pygame.quit()
-                    # fecha tudo!
-                    quit()
-                if evento.type == pygame.KEYDOWN:
-                    if evento.key == pygame.K_LEFT:
-                        movimentoX = -10
-                    elif evento.key == pygame.K_RIGHT:
-                        movimentoX = 10
-                if evento.type == pygame.KEYUP:
-                    if evento.key == pygame.K_LEFT or evento.key == pygame.K_RIGHT:
-                        movimentoX = 0
-            cesta_posicaoX = cesta_posicaoX + movimentoX
-
-            gameDisplay.fill(branco)
-            gameDisplay.blit(fundo, (0, 0))
-
-            mostrarCesta(cesta_posicaoX, cesta_posicaoY)
-
-            '''
-            mostrarAlfabeto(missile_posicaoX, missile_posicaoY)
-            missile_posicaoY = missile_posicaoY + missile_spped
-            '''
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def message_display(text):
+    largeText = pygame.font.Font("freesansbold.ttf", 115)
+    TextSurf, TextRect = text_objects(text, largeText)
+    TextRect.center = (tela_largura/2, tela_altura/2)
+    gameDisplay.blit(TextSurf, TextRect)
+    pygame.display.update()
+    time.sleep(3)
     game_loop()
+
+
+def dead():
+    message_display("Você Morreu")
+
+
+def escrePlacar(contador):
+    font = pygame.font.SysFont(None, 45)
+    text = font.render("Desvios: "+str(contador), True, branco)
+    gameDisplay.blit(text, (10, 30))
+
+
+def pegaObjeto(indice):
+    if indice==1:
+        return a
+    elif indice==2:
+        return b
+    elif indice==3:
+        return c
+    elif indice==4:
+        return um
+    elif indice==5:
+        return dois
+    elif indice==6:
+        return tres
+
+
+def game_loop():
+    cesta_posicaoX = 350
+    cesta_posicaoY = 450
+    movimentoX = 0
+    item_speed = 3
+    item_posicaoX = random.randrange(0, tela_largura)
+    item_posicaoY = -250
+    desvios = 0
+    item = pegaObjeto( random.randrange(1, 7) )
+    
+    while True:
+        # inicio - Interação do usuário
+        # event.get do pygame, devolve uma lista de eventos da janela
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                pygame.quit()
+                # fecha tudo!
+                quit()
+            if evento.type == pygame.KEYDOWN:
+                if evento.key == pygame.K_LEFT:
+                    movimentoX = -10
+                elif evento.key == pygame.K_RIGHT:
+                    movimentoX = 10
+            if evento.type == pygame.KEYUP:
+                if evento.key == pygame.K_LEFT or evento.key == pygame.K_RIGHT:
+                    movimentoX = 0
+        cesta_posicaoX = cesta_posicaoX + movimentoX
+
+        gameDisplay.fill(branco)
+        gameDisplay.blit(fundo, (0, 0))
+
+        mostrarCesta(cesta_posicaoX, cesta_posicaoY)
+
+
+        mostrarAlfabeto(item,item_posicaoX, item_posicaoY)
+        item_posicaoY = item_posicaoY + item_speed
+        if item_posicaoY > tela_altura:
+            item = pegaObjeto( random.randrange(1, 7) )
+            item_posicaoY = 0-86
+        
+        pygame.display.update()
+        clock.tick(60)
+
+
+        
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+game_loop()
